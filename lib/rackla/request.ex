a7087ecb6c0e301a3eb22a016652c80da4399cdc
Anton Fagerberg <anton@antonfagerberg.com>
2015-05-21 13:55:56 +0200

    Documented the Rackla.Request struct

--- lib/rackla/request.ex
+++ lib/rackla/request.ex
@@ -1,14 +1,35 @@
 defmodule Rackla.Request do
+  @moduledoc """
+  `Rackla.Request` is the struct used when doing advanced HTTP requests.
+
+  Required:
+  * `url` - The URL to call in the HTTP request.
+
+  Optional:
+  * `method` - HTTP verb, default: `:get`.
+  * `headers` - HTTP request headers, default: `%{}`.
+  * `body` - HTTP request body (payload), default: `""`.
+  * `options` - HTTP request specific options (see options below). These will
+  overwrite global options.
+
+  Options:
+  * `:connect_timeout` - Connection timeout limit (ms), default: `5_000`.
+  * `:receive_timeout` - Receive timeout limit (ms), default: `5_000`.
+  * `:insecure` - If true, SSL certificates will not be checked, default: `false`.
+  """
+
   @type t :: %__MODULE__{
-              method:   atom, 
-              url:      binary, 
-              headers:  %{}, 
-              body:     binary, 
+              method:   atom,
+              url:      binary,
+              headers:  %{},
+              body:     binary,
               options:  %{}}
-  
-  defstruct method:   :get, 
-            url:      "", 
-            headers:  %{}, 
-            body:     "", 
-            options:  %{}
+
+  defstruct method:   :get,
+            url:      "",
+            headers:  %{},
+            body:     "",
+            options:  %{connect_timeout: 5_000,
+                        receive_timeout: 5_000,
+                        insecure: false}
 end
