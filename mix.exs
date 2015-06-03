868040675aafef8cd11411a49a378c92ada8c77f
Anton Fagerberg <anton@antonfagerberg.com>
2015-06-03 09:51:18 +0200

    Set proper version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rackla.Mixfile do
   def project do
     [
       app: :rackla,
-      version: "0.1",
+      version: "0.1.0",
       elixir: "~> 1.0",
       deps: deps,
       escript: escript,
