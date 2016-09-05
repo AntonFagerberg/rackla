430f869b35b3cc0eaf7d557712f4b48c2aec34c7
Michael <justmichael@mail.ru>
2016-09-05 13:21:17 +0200

    Remove compilation warnings

--- lib/rackla/router_gen_server.ex
+++ lib/rackla/router_gen_server.ex
@@ -8,8 +8,11 @@ defmodule Rackla.RouterGenServer do
   end
 
   def init(_args) do
-    port = Application.get_env(:rackla, :port, 4000)
-    if is_binary(port), do: port = String.to_integer(port)
+    port = 
+      case Application.get_env(:rackla, :port, 4000) do
+        port when is_binary(port) -> String.to_integer(port)
+        port -> port
+      end
 
     Plug.Adapters.Cowboy.http(Router, [], port: port)
   end
