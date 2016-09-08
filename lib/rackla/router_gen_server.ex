59cbd4a5af168c6fadf16bc20371656d3f56f66a
Anton Fagerberg <anton@antonfagerberg.com>
2016-09-08 18:50:39 +0200

    Removed some whitespace.

--- lib/rackla/router_gen_server.ex
+++ lib/rackla/router_gen_server.ex
@@ -9,9 +9,7 @@ defmodule Rackla.RouterGenServer do
 
   def init(_args) do
     port = Application.get_env(:rackla, :port, 4000)
-    
-    port =
-      if is_binary(port), do: String.to_integer(port), else: port
+    port = if is_binary(port), do: String.to_integer(port), else: port
 
     Plug.Adapters.Cowboy.http(Router, [], port: port)
   end
